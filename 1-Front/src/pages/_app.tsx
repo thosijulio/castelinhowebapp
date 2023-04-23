372c1b3641ade7fba753e62be64eb5623d24e7de
thosijulio <thosijulio@gmail.com>
2023-04-23 10:41:54 -0300

    FEAT - configurando Provider do Chakra UI na raiz da aplicacao

--- 1-Front/src/pages/_app.tsx
+++ 1-Front/src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
+
+export default MyApp;
